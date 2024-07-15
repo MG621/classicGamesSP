cf7729ba793ef2bfec4334a1d2a9aabcef610c49
Mario A. Garza <86181558+MG621@users.noreply.github.com>
2024-07-15 15:43:34 -0500

    Simon game is now complete

--- simon.c
+++ simon.c
@@ -27,14 +27,14 @@
 
 /*{pal:"nes",layout:"nes"}*/
 const char PALETTE[32] = { 
-  0x03,			// screen color
+  0x0F,			// screen color
 
-  0x11,0x30,0x27,0x00,	// background palette 0
+  0x0F,0x30,0x27,0x00,	// background palette 0
   0x1C,0x20,0x2C,0x00,	// background palette 1
   0x00,0x10,0x20,0x00,	// background palette 2
   0x06,0x16,0x26,0x00,   // background palette 3
 
-  0x30,0x16,0x07,0x00,	// sprite palette 0
+  0x0F,0x16,0x07,0x00,	// sprite palette 0
   0x30,0x2A,0x0A,0x00,	// sprite palette 1
   0x30,0x21,0x01,0x00,	// sprite palette 2
   0x30,0x28,0x18	// sprite palette 3
@@ -45,10 +45,12 @@ const char PALETTE[32] = {
 int pattern[MAX_SCORE];
 int score;
 int rng_seed;
+int index;
 int i;
+int j;
+int k;
 int x;
 int y;
-int z;
 int frame_counter;
 bool game_over = false;
 
@@ -105,23 +107,80 @@ typedef struct {
   byte attr;
 } Sprite;
 
-// Define the sprite data for the Simon device
-Sprite simon_sprites[4] = {
-  {112, 104, 0x03, 0x01},  // Top-left (green)
-  {128, 104, 0x03, 0x00},  // Top-right (red)
-  {112, 120, 0x03, 0x03},  // Bottom-left (yellow)
-  {128, 120, 0x03, 0x02}   // Bottom-right (blue)
+Sprite simon_sprites[5][9] = {
+  {
+    {112, 72, 0x03, 0x01},  // Top-left (green)
+    {112, 80, 0x03, 0x01},  // Top-left (green)
+    {104, 80, 0x03, 0x01},  // Top-left (green)
+    //{96, 80, 0x03, 0x01},  // Top-left (green)
+    {112, 88, 0x03, 0x01},  // Top-left (green)
+    {104, 88, 0x03, 0x01},  // Top-left (green)
+    {96, 88, 0x03, 0x01},  // Top-left (green)
+    //{112, 96, 0x03, 0x01},  // Top-left (green)
+    {104, 96, 0x03, 0x01},  // Top-left (green)
+    {96, 96, 0x03, 0x01},  // Top-left (green)
+    {88, 96, 0x03, 0x01},  // Top-left (green)
+  },
+  {
+    {128, 72, 0x03, 0x00},  // Top-right (red)
+    {128, 80, 0x03, 0x00},  // Top-right (red)
+    {136, 80, 0x03, 0x00},  // Top-right (red)
+    //{144, 80, 0x03, 0x00},  // Top-right (red)
+    {128, 88, 0x03, 0x00},  // Top-right (red)
+    {136, 88, 0x03, 0x00},  // Top-right (red)
+    {144, 88, 0x03, 0x00},  // Top-right (red)
+    //{128, 96, 0x03, 0x00},  // Top-right (red)
+    {136, 96, 0x03, 0x00},  // Top-right (red)
+    {144, 96, 0x03, 0x00},  // Top-right (red)
+    {152, 96, 0x03, 0x00},  // Top-right (red)
+  },
+  {
+    //{112, 112, 0x03, 0x03},  // Bottom-left (yellow)
+    {104, 112, 0x03, 0x03},  // Bottom-left (yellow)
+    {96, 112, 0x03, 0x03},  // Bottom-left (yellow)
+    {88, 112, 0x03, 0x03},  // Bottom-left (yellow)
+    {112, 120, 0x03, 0x03},  // Bottom-left (yellow)
+    {104, 120, 0x03, 0x03},  // Bottom-left (yellow)
+    {96, 120, 0x03, 0x03},  // Bottom-left (yellow)
+    {112, 128, 0x03, 0x03},  // Bottom-left (yellow)
+    {104, 128, 0x03, 0x03},  // Bottom-left (yellow)
+    //{96, 128, 0x03, 0x03},  // Bottom-left (yellow)
+    {112, 136, 0x03, 0x03},  // Bottom-left (yellow)
+  },
+  {
+    //{128, 112, 0x03, 0x02},   // Bottom-right (blue)
+    {136, 112, 0x03, 0x02},   // Bottom-right (blue)
+    {144, 112, 0x03, 0x02},   // Bottom-right (blue)
+    {152, 112, 0x03, 0x02},   // Bottom-right (blue)
+    {128, 120, 0x03, 0x02},  // Bottom-right (blue)
+    {136, 120, 0x03, 0x02},  // Bottom-right (blue)
+    {144, 120, 0x03, 0x02},  // Bottom-right (blue)
+    {128, 128, 0x03, 0x02},   // Bottom-right (blue)
+    {136, 128, 0x03, 0x02},   // Bottom-right (blue)
+    {128, 136, 0x03, 0x02},   // Bottom-right (blue)
+  },
+  {
+    {120, 104, 0x02, 0x01},
+  }
 };
 
 // Function to draw the sprites
 void draw_simon_sprites() {
-  for (i = 0; i < 4; i++) {
-    oam_spr(simon_sprites[i].x, simon_sprites[i].y, simon_sprites[i].tile, simon_sprites[i].attr, i * 4);
+  for (i = 0; i < 5; i++) {
+    for(j = 0; j < 9; j++){
+      if(i == 4 && j == 1)
+        break;
+      index = i * 10 + j;
+      oam_spr(simon_sprites[i][j].x, simon_sprites[i][j].y, simon_sprites[i][j].tile, simon_sprites[i][j].attr, index * 4); 
+    }
   }
 }
 
 void flash_color(int color) {
-  oam_spr(simon_sprites[color].x, simon_sprites[color].y, simon_sprites[color].tile - 2, simon_sprites[color].attr, color * 4);
+  for(k = 0; k < 9; k++){
+     index = color * 10 + k;
+     oam_spr(simon_sprites[color][k].x, simon_sprites[color][k].y, simon_sprites[color][k].tile - 2, simon_sprites[color][k].attr, index * 4); 
+  }
 }
 
 void display_color(int input){
@@ -129,23 +188,15 @@ void display_color(int input){
   play_sound_effect(input);
   switch(input){
     case 1:
-      //vram_adr(NTADR_A(13,14));
-      //vram_write("GREEN", 5);
       flash_color(input - 1);
       break;
     case 2:
-      //vram_adr(NTADR_A(13,14));
-      //vram_write("RED", 3);
       flash_color(input - 1);
       break;
     case 3:
-      //vram_adr(NTADR_A(13,14));
-      //vram_write("YELLOW", 6);
       flash_color(input - 1);
       break;
     case 4:
-      //vram_adr(NTADR_A(13,14));
-      //vram_write("BLUE", 4);
       flash_color(input - 1);
       break;
     default:
@@ -154,7 +205,7 @@ void display_color(int input){
       break;
   }
   ppu_on_all();
-  delay(60);
+  delay(45);
   vram_adr(NTADR_A(13, 14)); // Clear the color
   vram_fill(0x20, 6);
   draw_simon_sprites();
@@ -186,9 +237,9 @@ void display_score() {
 
 void display_game_over(){
   ppu_off();
-  vram_adr(NTADR_A(11,9));
+  vram_adr(NTADR_A(11,20));
   vram_write("GAME OVER", 9);
-  vram_adr(NTADR_A(7,11));
+  vram_adr(NTADR_A(7,22));
   vram_write("PRESS B TO RESTART", 18);
   ppu_on_all();
 }
@@ -199,22 +250,61 @@ void init(){
   y = 0;
   frame_counter = 0;
   game_over = false;
-  for(z = 0; z < MAX_SCORE; z++){
-    pattern[z] = 0;
+  for(x = 0; x < MAX_SCORE; x++){
+    pattern[x] = 0;
   }
   display_score();
 }
 
 void restart(){
   ppu_off();
-  vram_adr(NTADR_A(11, 9));
+  vram_adr(NTADR_A(11, 20));
   vram_fill(0x20, 16);
-  vram_adr(NTADR_A(7, 11));
+  vram_adr(NTADR_A(7, 22));
   vram_fill(0x20, 32);
   init();  
   add_color();
   ppu_on_all();
   display_sequence();
+}
+
+void display_title(){
+  vram_adr(NTADR_A(13,2));
+  vram_write("SIMON", 5);
+}
+
+void title_screen() {
+    ppu_off();
+    display_title();
+    // Display start instructions
+    vram_adr(NTADR_A(6, 20));
+    vram_write("PRESS START TO BEGIN", 20);
+    vram_adr(NTADR_A(6, 25));
+    vram_write("(C)", 3);
+    vram_write("2024 MARIO GARZA", 16);
+    ppu_on_all();
+    while (1) {
+        char pad = pad_poll(0);
+      	if (pad & PAD_START) {
+          for(x = 0; x < 4; x++){
+            for(y = 0; y < 4; y++){
+              flash_color(y);
+              play_sound_effect(y + 1);
+              delay(3);
+              draw_simon_sprites();
+            } 
+          }
+          delay(15);
+          // Start the game when Start button is pressed
+          ppu_off();
+          break;
+        }
+        ppu_wait_frame();
+    }
+
+    // Clear the screen before starting the game
+    vram_adr(NTADR_A(0, 0));
+    vram_fill(0x20, 1024);
 }
 
 void win(){
@@ -251,11 +341,6 @@ void check_match(int input){
   }
 }
 
-void display_title(){
-  vram_adr(NTADR_A(13,2));
-  vram_write("SIMON", 5);
-}
-
 void seed_rng(int seed) {
   rng_seed = seed;
   srand(rng_seed);
@@ -274,6 +359,8 @@ void main(void)
   setup_graphics();
   apu_init();
   seed_rng(56);
+  draw_simon_sprites();
+  title_screen();
   init();
   ppu_off();
   display_title();
